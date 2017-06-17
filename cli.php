177751842ad95b741ba72078dea926799a40f12f
César Suárez <suarez.ortega.cesar@gmail.com>
2017-06-17 20:00:42 +0200

    Create commands with simple examples of locks

--- cli.php
+++ cli.php
@@ -12,13 +12,40 @@ use Symfony\Component\Lock\Store\RetryTillSaveStore;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use LockExamples\Cli\Application as LockApplication;
+use LockExamples\Resource\UnsafeSharedResource;
+use Simple\SHM\Block;
 
 $app = new LockApplication();
 
 $app->addStore('flock', new FlockStore(sys_get_temp_dir()));
 
-$app->command('simple:nolock [--test]', function (OutputInterface $output, Factory $factory, $input) {
-    $output->writeln('Hello world');
+$app->command('simple:nolock', function (OutputInterface $output, Factory $factory) {
+    $resource = new UnsafeSharedResource('simple:lock');
+    $resource->reset();
+
+    do {
+        $counter = $resource->read();
+        $output->writeln($counter);
+        $resource->write(++$counter);
+    } while(true);
+});
+
+$app->command('simple:lock', function (OutputInterface $output, Factory $factory) {
+    $resource = new UnsafeSharedResource('simple:lock');
+    $resource->reset();
+
+    $lock = $factory->createLock('simple:lock');
+
+    do {
+        $lock->acquire(true);
+        try {
+            $counter = $resource->read();
+            $output->writeln($counter);
+            $resource->write(++$counter);
+        } finally {
+            $lock->release();
+        }
+    } while(true);
 });
 
 
